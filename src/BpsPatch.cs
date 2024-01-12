fbac208dbca74a50dad3579a363c108b59dbdf47
Florent Castelli <florent.castelli@gmail.com>
2024-01-12 12:30:16 +0100

    Initial version

---
+++ src/BpsPatch.cs
@@ -0,0 +1,295 @@
+﻿using System.IO.Hashing;
+using System.Text;
+
+namespace BpsNet
+{
+    public class BpsPatch
+    {
+        private byte[] Actions {  get; init; }
+
+        enum ActionType
+        {
+            SourceRead = 0, TargetRead = 1, SourceCopy = 2, TargetCopy = 3
+        }
+
+        public int SourceSize { get; init; }
+        public int TargetSize { get; init; }
+        public String Metadata { get; init; }
+        public uint SourceChecksum { get; init; }
+        public uint TargetChecksum { get; init; }
+        public uint? PatchChecksum { get; private set; }
+
+        public BpsPatch(byte[] data)
+        {
+            if (data[0] != 'B' || data[1] != 'P' || data[2] != 'S' || data[3] != '1')
+                throw new InvalidDataException("Patch header is not BPS1");
+
+            int readIndex = 4;
+            SourceSize = (int)ReadNumber(data, ref readIndex);
+            TargetSize = (int)ReadNumber(data, ref readIndex);
+            int metadataSize = (int)ReadNumber(data, ref readIndex);
+            Metadata = System.Text.Encoding.UTF8.GetString(data, (int)readIndex, (int)metadataSize);
+            int actionStartIndex = readIndex + metadataSize;
+            Actions = new byte[data.Length - 12 - actionStartIndex];
+            Array.Copy(data, actionStartIndex, Actions, 0, Actions.Length);
+            readIndex = data.Length - 12;
+            SourceChecksum = ReadUint(data, ref readIndex);
+            TargetChecksum = ReadUint(data, ref readIndex);
+            PatchChecksum = ReadUint(data, ref readIndex);
+
+            uint computedPatchChecksum = Crc32.HashToUInt32(new ReadOnlySpan<byte>(data, 0, data.Length - 4));
+            if (PatchChecksum != computedPatchChecksum)
+            {
+                throw new InvalidDataException("Patch checksum is invalid");
+            }
+        }
+
+        private BpsPatch(byte[] source, byte[] target, string metadata, byte[] actions)
+        {
+            SourceSize = source.Length;
+            TargetSize = target.Length;
+            Metadata = metadata;
+            SourceChecksum = Crc32.HashToUInt32(source);
+            TargetChecksum = Crc32.HashToUInt32(target);
+            Actions = actions;
+            PatchChecksum = 0;
+        }
+
+        public byte[] GetBytes()
+        {
+            var memoryStream = new MemoryStream();
+            memoryStream.SetLength(128);
+            memoryStream.Write(new char[] { 'B', 'P', 'S', '1' }.Select(c => Convert.ToByte(c)).ToArray());
+            WriteNumber((uint)SourceSize, memoryStream);
+            WriteNumber((uint)TargetSize, memoryStream);
+            byte[] metadata = UTF8Encoding.UTF8.GetBytes(Metadata);
+            WriteNumber((uint)metadata.Length, memoryStream);
+
+            var variableHeaderSize = memoryStream.Position;
+
+            var hasher = new Crc32();
+            var header = memoryStream.ToArray();
+            hasher.Append(new ReadOnlySpan<byte>(header, 0, (int)variableHeaderSize));
+
+            memoryStream.SetLength(variableHeaderSize + metadata.Length + Actions.Length + 12);
+            memoryStream.Write(metadata);
+            hasher.Append(metadata);
+            memoryStream.Write(Actions);
+            hasher.Append(Actions);
+            var sourceChecksumBytes = UintToByte(SourceChecksum);
+            memoryStream.Write(sourceChecksumBytes);
+            hasher.Append(sourceChecksumBytes);
+            var targetChecksumBytes = UintToByte(TargetChecksum);
+            memoryStream.Write(targetChecksumBytes);
+            hasher.Append(targetChecksumBytes);
+
+            memoryStream.Write(UintToByte(hasher.GetCurrentHashAsUInt32()));
+
+            return memoryStream.ToArray();
+        }
+
+        public byte[] Apply(byte[] source)
+        {
+            byte[] target = new byte[TargetSize];
+            int outputOffset = 0;
+            int sourceRelativeOffset = 0;
+            int targetRelativeOffset = 0;
+
+            int actionIndex = 0;
+            int maxActionIndex = Actions.Length;
+            while (actionIndex < maxActionIndex)
+            {
+                uint data = ReadNumber(Actions, ref actionIndex);
+                uint command = data & 3;
+                uint length = (data >> 2) + 1;
+                switch((ActionType)command)
+                {
+                    case ActionType.SourceRead:
+                        while(length-- > 0)
+                        {
+                            target[outputOffset] = source[outputOffset];
+                            outputOffset++;
+                        }
+                        break;
+                    case ActionType.TargetRead:
+                        while(length-- > 0)
+                        {
+                            target[outputOffset++] = Actions[actionIndex++];
+                        }                               
+                        break;
+                    case ActionType.SourceCopy:
+                        data = ReadNumber(Actions, ref actionIndex);
+                        if ((data & 1) == 0)
+                            sourceRelativeOffset += (int)(data >> 1);
+                        else
+                            sourceRelativeOffset -= (int)(data >> 1);
+                        while(length-- > 0)
+                        {
+                            target[outputOffset++] = source[sourceRelativeOffset++];
+                        }
+                        break;
+                    case ActionType.TargetCopy:
+                        data = ReadNumber(Actions, ref actionIndex);
+                        if ((data & 1) == 0)
+                            targetRelativeOffset += (int)(data >> 1);
+                        else
+                            targetRelativeOffset -= (int)(data >> 1);
+                        while (length-- > 0)
+                        {
+                            target[outputOffset++] = target[targetRelativeOffset++];
+                        }
+                        break;
+                }
+            }
+            return target;
+        }
+
+        static private uint ReadNumber(byte[] data, ref int readIndex)
+        {
+            ulong result = 0, shift = 1;
+            while (true)
+            {
+                byte x = data[readIndex++];
+                result += (byte)(x & 0x7f) * shift;
+                if ((x & (byte)0x80) != 0) break;
+                shift <<= 7;
+                result += shift;
+
+                if (result > UInt32.MaxValue)
+                    throw new InvalidDataException("Number is out of allowed range");
+            }
+            return (uint)result;
+        }
+
+        static void WriteNumber(uint number, Stream data)
+        {
+            while(true)
+            {
+                byte x = (byte)(number & 0x7f);
+                number >>= 7;
+                if (number == 0)
+                {
+                    data.WriteByte((byte)(x | 0x80));
+                    break;
+                }
+                data.WriteByte(x);
+                number--;
+            }
+        }
+
+        static private uint ReadUint(byte[] data, ref int readIndex)
+        {
+            uint result = data[readIndex] + (uint)(data[readIndex + 1] << 8) + (uint)(data[readIndex + 2] << 16) + (uint)(data[readIndex + 3] << 24);
+            readIndex += 4;
+            return result;
+        }
+
+        static private byte[] UintToByte(uint value)
+        {
+            byte[] result = new byte[4];
+            result[0] = (byte)value;
+            result[1] = (byte)(value >> 8);
+            result[2] = (byte)(value >> 16);
+            result[3] = (byte)(value >> 24);
+            return result;
+        }
+
+        record PatchAction(ActionType Type, int Length, List<byte> Data, int RelativeOffset);
+
+        static public BpsPatch Create(byte[] sourceData, byte[] targetData, string metadata)
+        {
+            var patchActions = new List<PatchAction>();
+
+            /* references to match original beat code */
+            var sourceSize = sourceData.Length;
+            var targetSize = targetData.Length;
+            var Granularity = 1;
+
+            var targetRelativeOffset = 0;
+            var outputOffset = 0;
+            var targetReadLength = 0;
+
+            var targetReadFlush = () => 
+            {
+                if (targetReadLength > 0)
+                {
+                    var action = new PatchAction(ActionType.TargetRead, targetReadLength, new(), 0);
+                    patchActions.Add(action);
+	        		var offset = outputOffset - targetReadLength;
+			        while(targetReadLength > 0) {
+				        action.Data.Add(targetData[offset++]);
+				        targetReadLength--;
+			        }
+                }
+    	    };
+
+            while (outputOffset < targetSize)
+            {
+                var sourceLength = 0;
+                for (var n = 0; outputOffset + n < Math.Min(sourceSize, targetSize); n++)
+                {
+                    if (sourceData[outputOffset + n] != targetData[outputOffset + n]) break;
+                    sourceLength++;
+                }
+
+                var rleLength = 0;
+                for (var n = 1; outputOffset + n < targetSize; n++)
+                {
+                    if (targetData[outputOffset] != targetData[outputOffset + n]) break;
+                    rleLength++;
+                }
+
+                if (rleLength >= 4)
+                {
+                    //write byte to repeat
+                    targetReadLength++;
+                    outputOffset++;
+                    targetReadFlush();
+
+                    //copy starting from repetition byte);
+                    var relativeOffset = (outputOffset - 1) - targetRelativeOffset;
+                    patchActions.Add(new PatchAction(ActionType.TargetCopy, rleLength, [], relativeOffset));
+                    outputOffset += rleLength;
+                    targetRelativeOffset = outputOffset - 1;
+                }
+                else if (sourceLength >= 4)
+                {
+                    targetReadFlush();
+                    patchActions.Add(new PatchAction(ActionType.SourceRead, sourceLength, [], 0));
+                    outputOffset += sourceLength;
+                }
+                else
+                {
+                    targetReadLength += Granularity;
+                    outputOffset += Granularity;
+                }
+            }
+
+            targetReadFlush();
+
+            var memoryStream = new MemoryStream();
+            memoryStream.SetLength(1 * 1024 * 1024);
+
+            foreach (var action in patchActions)
+            {
+                while (memoryStream.Position + action.Data.Count + 64 > memoryStream.Length)
+                {
+                    memoryStream.SetLength(memoryStream.Length + 1024 * 1024);
+                }
+                WriteNumber((((uint)action.Length - 1) << 2) + (uint)action.Type, memoryStream);
+                if (action.Type == ActionType.TargetRead) {
+                    memoryStream.Write(action.Data.ToArray());
+                }
+                else if(action.Type == ActionType.SourceCopy || action.Type == ActionType.TargetCopy)
+                {
+                    WriteNumber((uint)(Math.Abs(action.RelativeOffset) << 1) + (uint)(action.RelativeOffset < 0 ? 1 : 0), memoryStream);
+                }
+            }
+
+            memoryStream.SetLength(memoryStream.Position);
+
+            return new BpsPatch(sourceData, targetData, metadata, memoryStream.ToArray());
+        }
+    }
+    
+}
---
+++ tests/BpsPatchTest.cs
@@ -0,0 +1,107 @@
+using BpsNet;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.IO.Hashing;
+using System.Runtime.CompilerServices;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace BspNetTest
+{
+    [TestClass]
+    public class BpsPatchTest
+    {
+        [TestMethod]
+        public void ChecksumTest()
+        {
+            byte[] data = File.ReadAllBytes(Path.Combine(new string[] { ProjectSourcePath.Value, "data", "gradient.bps" }));
+            BpsPatch p = new BpsPatch(data);;
+        }
+
+        [TestMethod]
+        public void InvalidMagicTest()
+        {
+            byte[] data = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            Assert.ThrowsException<InvalidDataException>(() => new BpsPatch(data));
+        }
+
+        [TestMethod]
+        public void OutOfRangeNumberTest()
+        {
+            byte[] data = { (byte)'B', (byte)'P', (byte)'S', (byte)'1', 4, 5, 6, 7, 8, 9 };
+            Assert.ThrowsException<InvalidDataException>(() => new BpsPatch(data));
+        }
+
+        [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "WriteNumber")]
+        extern static void BpsPatchWriteNumber(BpsPatch @this, uint value, Stream stream);
+        [UnsafeAccessor(UnsafeAccessorKind.StaticMethod, Name = "ReadNumber")]
+        extern static uint BpsPatchReadNumber(BpsPatch @this, byte[] data, ref int readIndex);
+
+        [TestMethod]
+        public void MaxNumberTest()
+        {
+            var memoryStream = new MemoryStream(16);
+            BpsPatchWriteNumber(null!, (uint)Int32.MaxValue, memoryStream);
+            var data = memoryStream.ToArray();
+            int readIndex = 0;
+            uint number = BpsPatchReadNumber(null!, data, ref readIndex);
+            Assert.AreEqual((uint)Int32.MaxValue, number);
+        }
+
+        [TestMethod]
+        public void TestRoundTripString()
+        {
+            byte[] original = UTF8Encoding.UTF8.GetBytes("123456700000000000012345678");
+            byte[] target = UTF8Encoding.UTF8.GetBytes("123456700000001111123456999999abc0000000000");
+
+            AssertRoundTrip(original, target, "metadata with UTF-8: Hur mår du? Ça va très bien !") ;
+        }
+
+        [TestMethod]
+        public void TestRoundTripStringGradient()
+        {
+            byte[] original = File.ReadAllBytes(Path.Combine(new string[] { ProjectSourcePath.Value, "data", "gradient1.png" }));
+            byte[] target = File.ReadAllBytes(Path.Combine(new string[] { ProjectSourcePath.Value, "data", "gradient2.png" }));
+
+            AssertRoundTrip(original, target, "metadata with UTF-8: Hur mår du? Ça va très bien !");
+        }
+
+        [TestMethod]
+        public void TestExistingGradientPatch()
+        {
+            byte[] original = File.ReadAllBytes(Path.Combine(new string[] { ProjectSourcePath.Value, "data", "gradient1.png" }));
+            byte[] target = File.ReadAllBytes(Path.Combine(new string[] { ProjectSourcePath.Value, "data", "gradient2.png" }));
+            byte[] patchBytes = File.ReadAllBytes(Path.Combine(new string[] { ProjectSourcePath.Value, "data", "gradient.bps" }));
+
+            var patch = new BpsPatch(patchBytes);
+            byte[] patchedBytes = patch.Apply(original);
+            CollectionAssert.AreEqual(target, patchedBytes);
+        }
+
+
+        public void AssertRoundTrip(byte[] original, byte[] target, string metadata)
+        {
+            BpsPatch generatedPatch = BpsPatch.Create(original, target, metadata);
+            byte[] generatedPatchBytes = generatedPatch.GetBytes();
+
+            var generatedPatchRoundTrip = new BpsPatch(generatedPatchBytes);
+            var patchedRoundTrip = generatedPatchRoundTrip.Apply(original);
+            CollectionAssert.AreEqual(target, patchedRoundTrip);
+            Assert.AreEqual(generatedPatchRoundTrip.Metadata, metadata);
+        }
+    }
+
+    internal static class ProjectSourcePath
+    {
+        private static string? lazyValue;
+        public static string Value => lazyValue ??= calculatePath();
+
+        private static string calculatePath()
+        {
+            string pathName = GetSourceFilePathName();
+            return Path.GetDirectoryName(pathName) ?? "";
+        }
+        public static string GetSourceFilePathName([CallerFilePath] string? callerFilePath = null) => callerFilePath ?? "";
+    }
+}
